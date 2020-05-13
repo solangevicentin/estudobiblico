cca059a1d41a384f78108f9035286b3dfdd41bb6
solangevicentin <solange-vicentin@outlook.com>
2020-05-13 01:43:31 +0000

    Versão CRUD em memória

--- server.js
+++ server.js
@@ -1,58 +1,55 @@
 const express = require('express');
-
+ 
 const server = express();
-
+ 
 server.use(express.json());
-
-const Leitura = [
-    {livro:'Gênesis',capitulo: 3, versiculo: 16 ,lido:'sim'},
+ 
+const livros = [
+     {livro:'Gênesis',capitulo: 3, versiculo: 16 ,lido:'sim'},
     {livro:'Êxodo', capitulo:11, versiculo: 18, lido:'sim'}
 ]
-
-server.get('/livro', function(request, response) {
-    response.json(Leitura);
-})
-
-server.post('/livro', function(request, response){
-
-const livro= request.body.livro;
-const capitulo=request.body.capitulo;
-const versiculo=request.body.versiculo;
-const lido=request.body.lido;
-
-Leitura.push({livro: livro, capitulo:capitulo, versiculo:versiculo, lido:lido});
-response.status(204).send();
-})
-
-server.put('/livro/:id', function(request,response){
-const {id}=request.params;
-const {livro,capitulo,versiculo,lido}=request.body;
-
-for(let i= 0;i<Leitura.lenght; i++) {
-  if(leitura[i].livro==id){
-      leitura[i].livro=livro;
-      leitura[i].capitulo=capitulo;
-      leitura[i].versiculo=versiculo;
-      leitura[i].lido=lido;
-      break;
-
-  }
-}
  
-return response.status(204).send();
-
+server.get('/livro', function(request, response) {
+    response.json(livros);
 })
-
-server.delete('/livro/:id',function(request, response){
-    const id=request.params.id;
-
-    for(let i= 0;i<Leitura.lenght; i++) {
-  if(leitura[i].livro==id){
-     leitura.splice(i,1);
-     break;
+ 
+server.post('/livro', function(request, response) {
+ 
+    const {livro, capitulo, versiculo, lido} = request.body;
+ 
+    livros.push({livro, capitulo, versiculo, lido});
+    response.status(204).send();
+})
+ 
+server.put('/livro/:id', function(request, response){
+    const id = request.params.id;
+    const {livro, capitulo, versiculo, lido} = request.body;
+ 
+    for(let i = 0; i < livros.length; i++){
+        if(livros[i].livro == id) {
+            livros[i].livro = livro;
+            livros[i].capitulo = capitulo;
+            livros[i].versiculo = versiculo;
+            livros[i].lido = lido;
+            break;
+        }
     }
-  }
-
- return response.status(204).send();
+ 
+    return response.status(204).send();
 })
+ 
+server.delete('/livro/:id', function(request, response) {
+ 
+    const id = request.params.id;
+ 
+    for(let i = 0; i < livros.length; i++) {
+        if(livros[i].livro == id) {
+            livros.splice(i, 1);
+            break;
+        }
+    }
+ 
+    return response.status(204).send();
+})
+ 
 server.listen(process.env.PORT || 3000);
